c58c9500d910875c5fa237025aac4ab1b5c8742e
aryark <aryark3112@gmail.com>
2023-07-19 09:18:38 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _19July
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\\";
+            string fname;
+            Console.WriteLine("Enter File name");
+            fname = Console.ReadLine();
+            string fPath = path + fname;
+            if(File.Exists(fPath))
+            {
+                Console.WriteLine("The file already exist!!!");
+            }
+            else
+            {
+                File.Create(fPath);
+                Console.WriteLine("File Created !!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
